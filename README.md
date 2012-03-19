d1f5a127c32f955899994a0f02204c06e5609483
vaddi <vaddi@exigem.de>
2012-03-19 14:31:41 +0100

    Clean Up Folder Structure

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ Make sure the "xml" Folder is writeable by Webserver (often www-data) otherwise 
 On Empty xml Folder you will get a Link to the createArticle Site (You will be loged in first). Now you can create Booknotes to your own fits. You can remind yourself when you have borred a book to someone. You can simple Organize hints like ISBN, Color or Links. 
 Shortform, Content, Index and Colophone (all textforms) will be formated by a simple BBCode style:
 
-1.  &#91;b&#93;TEXT&#91;/b&#93; => &lt;b&gt;TEXT&lt;/b&gt;, All simple HTML-Tags (e.g. "i" or "u")
+1.  &#91;b&#93;TEXT&#91;/b&#93; => &lt;b&gt;TEXT&lt;/b&gt;, All simple HTML-Tags (also "i" or "u")
 2.  &#91;url=TEXT1&#93;TEXT2&#91;/url&#93; => &lt;a href="TEXT1"&gt;TEXT2&lt;/a&gt;, Textlink or URL
 3.  &#91;img&#93;TEXT&#91;/img&#93; => &lt;img src="TEXT" alt="TEXT" /&gt;, Images
 4.  All URLs (http://domain.tld/) will be formated to clickable href links
@@ -36,7 +36,7 @@ Shortform, Content, Index and Colophone (all textforms) will be formated by a si
 ## Idea ##
 
 Based on a small [PHP XML CMS][] writen by Tom Myer and a [PHP-Libary][] by Alexandre Alapetite that make OML automaticly usable on PHP4 or PHP5 Webservers.
-The Baseidea should be a simple Note for a [O'Reilly Media Books][]. Beside the maincoding the Idea grown up by more and more Features:
+The Baseidea should be a simple Note for a [O'Reilly Books][]. Beside the maincoding the Idea grown up by more and more Features:
 
 - write down a native linkadress, it will be saved in same format, but will be displayed as a clickable href link
 - BBCode Shortcode style to get a litle bit more formating
@@ -44,9 +44,9 @@ The Baseidea should be a simple Note for a [O'Reilly Media Books][]. Beside the 
 - Have to be very userfriendly by simple Structure and Usage
 
 
-## OnGoing ##
+## On Going ##
 
-Nice to have things:
+Would be nice to have:
 
 - A nice Idea for index Pageination for better viewing a lot of Books
 - CodeMirror should be for a better Syntax highlighting on Input
@@ -63,11 +63,11 @@ Nice to have things:
 
 ## Credits ##
 
-1.  [PHP XML CMS][]
-2.  [PHP-Libary][]
-3.  [O'Reilly Media Books][]
+1.  [PHP XML CMS][] writen by Tom Myer
+2.  [PHP-Libary][] writen by Alexandre Alapetite
+3.  [O'Reilly Books][] A lot of O'Reilly Books
 
 [PHP XML CMS]: http://www.sitepoint.com/management-system-php/
 [PHP-Libary]: http://alexandre.alapetite.fr/doc-alex/domxml-php4-php5/
-[O'Reilly Media Books]: http://oreilly.com/
+[O'Reilly Books]: http://oreilly.com/
 
--- index.php
+++ index.php
@@ -1,77 +1,21 @@
-<!DOCTYPE html>
-<html lang="de">
-
 <?php
 if (PHP_VERSION>='5')
- require_once('domxml-php4-to-php5.php');
-
-function extractText($array){    
- if(count($array) <= 1){ 
-   $value = "";   
-   //we only have one tag to process!    
-   for ($i = 0; $i<count($array); $i++){    
-     $node = $array[$i];    
-     $value = $node->get_content();    
-   }    
-   return $value;    
- }      
-     
-}      
+ require_once('inc/domxml-php4-to-php5.php');
+include("inc/functions.php"); 
+include("inc/head.php"); 
 ?>    
-
-<head>    
-<title>ORlib - Media Library | OML</title>    
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="css/style.css" type="text/css" rel="stylesheet" media="screen" />
-<script src="js/datum.js" type="text/javascript"></script>
-<script type="text/javascript">
-if (navigator.userAgent.toLowerCase().indexOf('chrome')!=-1){
-  document.write('<link rel="stylesheet" type="text/css" href="css/chrome.css"/>');
-}
-</script>
-</head>    
    
 <body>    
 
 <div id="wrap">
 
-<header>
-  <div class="search">
-    <form name="search" method="post" action="searchArticles.php">    
-     <input name="search" type="text" id="search" placeholder="Suchen">    
-     <button name="Search" type="submit" id="Search">Suchen</button>    
-    </form>  
-    <span class="login"><a href="adminindex.php">Admin</a></span>
-  </div>
-  <div id="headnav"> 
-    <h1>ORlib - Media Library | OML</h1> 
-  </div>
-
-  <div id="header-time">
-     <script type="text/javascript">writeclock()</script> 
-     <noscript><p></p></noscript>
-   </div>
-
-</header>
+<?php include("inc/header.php") ?>
 
 <div id="content_small">
 
 <article>
 
-<header>
-<h1>Zweck</h1>
-</header>
-<p>Ziel ist es eine kleine virtuelle B&uuml;cherei als erg&auml;nzendes Material zu seinen B&uuml;chern. So kann man sich Platzsparend Notizen anlegen und in diese mit der Suche auch schnell durchst&ouml;bern. Basierend auf einem <a href="http://www.sitepoint.com/management-system-php/">PHP XML CMS</a> von Tom Myer und einer <a href="http://alexandre.alapetite.fr/doc-alex/domxml-php4-php5/">PHP-Libary</a> von Alexandre Alapetite zur Umwandlung von PHP4 xml Aufrufen in PHP5 sofern der Server PHP5 verwendet.</p>
-
-<header>
-<h1>Funktion</h1>
-</header>
-<p>Basierend auf einer einfachen xml Datei pro Buch, kann man sich z.B. Links, Notizen oder andere Informationen zu seinem Buch anlegen und speichern. Mit Bekannten oder Freunden kann man sich so auf einfachem Wege diese Informationen austauschen und teilen.</p>
-
-<header>
-<h1>Verliehen?</h1>
-</header>
-<p>Eine kleine hilfe f&uuml;r die jenigen die Ihre B&uuml;cher &ouml;fter Verleihen, man kann den Namen und Ausleihdatum als Merkhilfe anlegen bzw. Auslesen. In der Bücherregalansicht sind verliehene B&uuml;cher Transparent hinterlegt, so sieht man auf den ersten Blick wie viele sich noch im eigenen Regal befinden sollten.</p>
+<?php include("inc/index_content.php"); ?>
 
 </article>
 </div>  <!-- close #content -->   
@@ -82,21 +26,20 @@ if (navigator.userAgent.toLowerCase().indexOf('chrome')!=-1){
 
 // Wenn noch kein Buch im index link um neues zu erstellen anzeigen
 $verzeichnis_raw = './xml/';
-$verzeichnis = openDir($verzeichnis_raw);  
 $verzeichnis_glob = glob($verzeichnis_raw . '*.xml');
-$dh = $verzeichnis_glob;
 $fileCount = 0;    
-if (count($dh) == $fileCount) {
- echo '<li>'."\n"; 
- echo '  <a href="createArticle.php">'."\n";  
- echo '  <span class="spine_publisher">ORlib</span>'."\n";   
- echo '  <div class="inlinebg" style="background:#BBBBBB;">'."\n";
- echo '    <span class="spine_author">Author</span>'."\n"; 
- echo '    <span class="spine_title">Erstes Buch erstellen</span>'."\n"; 
- echo '  </div>'."\n";
- echo '  <span class="spine_edition">v 0.1</span>'."\n";
- echo '</a>'."\n";    
- echo '</li>'."\n"; 
+
+if (count($verzeichnis_glob) == $fileCount) {
+  echo '<li>'."\n"; 
+  echo '  <a href="inc/createArticle.php">'."\n";  
+  echo '  <span class="spine_publisher">ORlib</span>'."\n";   
+  echo '  <div class="inlinebg" style="background:#BBBBBB;">'."\n";
+  echo '    <span class="spine_author">Author</span>'."\n"; 
+  echo '    <span class="spine_title">Erstes Buch erstellen</span>'."\n"; 
+  echo '  </div>'."\n";
+  echo '  <span class="spine_edition">v 0.1</span>'."\n";
+  echo '</a>'."\n";    
+  echo '</li>'."\n"; 
 }
 
 foreach($verzeichnis_glob as $key => $file){
@@ -129,12 +72,24 @@ foreach($verzeichnis_glob as $key => $file){
  $abstract = extractText($ab_array);
  $abstract = htmlspecialchars("$abstract", ENT_NOQUOTES, "UTF-8"); 
    
+ if (count($verzeichnis_glob) == "1") {
+    echo '<li>'."\n"; 
+    echo '  <a href="inc/editArticle.php?file=' . $fileraw . '"  class="verliehen">'."\n";  
+    echo '  <span class="spine_publisher">ORlib</span>'."\n";   
+    echo '  <div class="inlinebg" style="background:' . $color . ';">'."\n";
+    echo '    <span class="spine_author">' . $authors . '</span>'."\n"; 
+    echo '    <span class="spine_title">' . $headline . '<br /> ist noch nicht online</span>'."\n"; 
+    echo '  </div>'."\n";
+    echo '  <span class="spine_edition">' . $version . '</span>'."\n";
+    echo '</a>'."\n";    
+    echo '</li>'."\n"; 
+ }
    
  if ($status != "online"){    
    continue;    
  }    
  echo '<li>'."\n"; 
- echo '  <a href="showArticle.php?file=' . $fileraw . '"  class="' . $lent . '">'."\n";  
+ echo '  <a href="inc/showArticle.php?file=' . $fileraw . '"  class="' . $lent . '">'."\n";  
  echo '  <span class="spine_publisher">ORlib</span>'."\n";   
  echo '  <div class="inlinebg" style="background:' . $color . ';">'."\n";
  echo '    <span class="spine_author">' . $authors . '</span>'."\n"; 
@@ -147,12 +102,11 @@ foreach($verzeichnis_glob as $key => $file){
  $fileCount++;    
 }    
 ?>    
- 
 
 </ul>
 </nav>
 
-<?php include("footer.php") ?>
+<?php include("inc/footer.php") ?>
 
 </div><!-- close #wrap -->
 <div class="clear"></div>
